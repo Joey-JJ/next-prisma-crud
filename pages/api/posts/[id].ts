90432ec898924c35bf91b3da7086f6474c9ea3db
Joey van der Valk <joeyvandervalk@gmail.com>
2022-09-07 12:53:17 +0200

    change to api file extensions

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,7 @@
+// Handler will be used for updating and deleting posts
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {};
+
+export default handler;
