ff5f378fe53c0aa504f2beba4171c916a93e9fa1
Joey van der Valk <joeyvandervalk@gmail.com>
2022-09-11 14:16:41 +0200

    Small change to blog card design

--- components/Blog/BlogItem.tsx
+++ components/Blog/BlogItem.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import type { Post } from "@prisma/client";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import BlogEditingForm from "./BlogEditingForm";
 import classes from "../../styles/BlogItem.module.css";
 
@@ -43,14 +44,26 @@ const BlogItem: React.FC<Props> = (props) => {
   return (
     <>
       <div className={classes.container}>
-        <h1 className={classes.title}>{`${post.title}`}</h1>
-        <h3 className={classes.author}>{`By ${post.author}`}</h3>
-        <p className={classes.body}>{`${post.body}`}</p>
-        <div className={classes.buttons}>
-          <button onClick={onDelete}>Delete</button>
-          <button onClick={() => setIsEditing((prevstate) => !prevstate)}>
-            Edit
-          </button>
+        <Image
+          src={
+            "https://images.unsplash.com/photo-1661846601758-28ce69e3d480?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
+          }
+          width={200}
+          height={200}
+          alt="avatar"
+        />
+        <div className={classes["content-container"]}>
+          <div className={classes["card-titles"]}>
+            <h1 className={classes.title}>{`${post.title}`}</h1>
+            <h3 className={classes.author}>{`By ${post.author}`}</h3>
+          </div>
+          <p className={classes.body}>{`${post.body}`}</p>
+          <div className={classes.buttons}>
+            <button onClick={onDelete}>Delete</button>
+            <button onClick={() => setIsEditing((prevstate) => !prevstate)}>
+              Edit
+            </button>
+          </div>
         </div>
       </div>
     </>
